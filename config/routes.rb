18d587ba13b256a43a69262361a858483b4d9a2f
Allan-Ngugi <blessedallan1@gmail.com>
2022-12-06 09:22:50 +0300

    Updated json response

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :project_managers
-  resources :developers
+  resources :projects, only:[:index, :show] 
+  resources :project_managers,only: [:index, :show]
+  resources :developers, only:[:index, :show, :create]
 
   # Defines the root path route ("/")
   # root "articles#index"
