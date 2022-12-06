475c662236ebf9f67f89b59c339742c91f9c8508
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-06 15:19:26 +0300

    Modify file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   resources :projects, only:[:index, :show] 
   resources :project_managers, only: [:index, :show]
   resources :developers, only:[:index, :show, :create]
-  get '/login/developer', to: 'developer_sessions#create'
+  post '/login/developer', to: 'developer_sessions#create'
   delete '/logout/logout', to: 'developer_sessions#destroy'
-  get '/login/project_manager', to: 'project_manager_sessions#create'
+  post '/login/project_manager', to: 'project_manager_sessions#create'
   delete '/logout/project_manager', to: 'project_manager_sessions#destroy'
+  get '/me', to: 'user#show'
 
   # Defines the root path route ("/")
   # root "articles#index"
