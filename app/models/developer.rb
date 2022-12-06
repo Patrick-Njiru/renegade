890699624a44b940fcf6ffbe1d3e3a35b3071b64
Allan-Ngugi <blessedallan1@gmail.com>
2022-12-06 09:00:50 +0300

    Modified the model and controllers

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,6 @@
 class Developer < ApplicationRecord
+    has_secure_password
+    validates :password_digest, length: {maximum: 6}
+    has_many :projects
+    has_many :projects, through: :project_managers 
 end
