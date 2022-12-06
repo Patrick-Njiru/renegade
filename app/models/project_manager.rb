6ca6f31d689dd654c115b671a5ffea8d99a19540
Allan-Ngugi <blessedallan1@gmail.com>
2022-12-06 09:01:26 +0300

    Modified the managers model and controllers

--- app/models/project_manager.rb
+++ app/models/project_manager.rb
@@ -1,2 +1,6 @@
 class ProjectManager < ApplicationRecord
+    has_secure_password
+    validates :password, length, {maximum: 6}
+    has_many  :developers
+    has_many  :projects through: :developers
 end
