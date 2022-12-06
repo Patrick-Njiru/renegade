b98c0f02dbea4ab04e60280c77eaa2c8d54cf185
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-06 10:58:31 +0300

    Modify routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/login/developer', to: 'developer_sessions#create'
+  delete '/logout/logout', to: 'developer_sessions#destroy'
+  get '/login/project_manager', to: 'project_manager_sessions#create'
+  delete '/logout/project_manager', to: 'project_manager_sessions#destroy'
+
   resources :projects
   resources :project_managers
   resources :developers
