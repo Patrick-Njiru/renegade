00c09008a1fe9d4da8ad68dd9d0cf26643bf860c
Allan-Ngugi <blessedallan1@gmail.com>
2022-12-06 09:06:37 +0300

    Updated r/ships

--- app/models/project_manager.rb
+++ app/models/project_manager.rb
@@ -1,6 +1,6 @@
 class ProjectManager < ApplicationRecord
     has_secure_password
     validates :password, length, {minimum: 6}
-    has_many  :developers
-    has_many  :projects through: :developers
+    has_many  :projects
+    has_many  :developers through: :projects
 end
