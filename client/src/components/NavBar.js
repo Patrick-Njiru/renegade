722177beadb585f28f721050aea16d8fe09f7963
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-12-06 11:21:08 +0300

    modified navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,7 +8,7 @@ function Navbar() {
         <div className='nav-container'>
             <div className='nav-upper'>
                 <div className='nav-heading'>
-                    <h1 className='company-name'>Renegade</h1>
+                    <h1 clacompany-namessName='company-name'><a href='/home'> Renegade</a></h1>
                     <div className='nav-brand'>
                         <img src='https://cdn-icons-png.flaticon.com/512/149/149071.png' alt=''></img>
                         <i class="fa fa-user-circle" aria-hidden="true"></i>
@@ -16,11 +16,16 @@ function Navbar() {
                         <h3 className='dev-id'>Developer</h3>
                     </div>
                 </div>
+
                 <div className='nav-dash'>
                       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2ucvuvELzjICuBFdrCDZvjEZQaJ1eavD_XQ&usqp=CAU' alt=''></img>
-                      <h2 className='dash-name'>Dashboard</h2>
+                      <h2><a href='/dashboard'>Dashboard</a></h2>
                   </div>
             </div>
+        </div>
+        
+        <div className='project-container'>
+            <h1>I am here</h1>
         </div>
     </>
   )
