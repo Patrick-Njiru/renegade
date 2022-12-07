c119007f7a98d6b574649a638b27aff36587f8d2
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-07 16:01:37 +0300

    Modify file.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div id='homepage'>
       <h2>
-        Please select your position in the organization: <br />
+        Welcome to Renegade. <br /> Are you a developer or project manager ? Please select one to continue <br />
       </h2>
       <NavLink to='/login/developer'>I am a Developer</NavLink> <br />
       <NavLink to='/login/project_manager'>I am a Project Manager</NavLink>
