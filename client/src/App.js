a282e756e2bf3255095a493d854b0adf0a2bbb16
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-07 14:50:31 +0300

    Import child components and add routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import Home from "./components/Home";
-import Developer from './components/Developer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
 import Manager from './components/Manager';
-import NavBar from './components/NavBar'
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import Developer from './components/Developer';
+import SignupForm from './components/SignupForm';
+import ProjectManagerLoginForm from './components/ProjectManagerLoginForm';
+import DeveloperLoginForm from './components/DeveloperLoginForm.js';
+// import NavBar from './components/NavBar'
 
 function App() {
   return (
       <Router>
         {/* <NavBar /> */}
-            <Switch>
-              <Route exact  path="/"> <Home /> </Route>
-              <Route exact path="/developer"> <Developer /> </Route>
-              <Route exact path="/manager"> <Manager /> </Route>
-            </Switch>
+            <Routes>
+              <Route exact path="/" element={<Home />} /> 
+              <Route path="/developer" element={<Developer />} />
+              <Route path="/project_manager" element={<Manager />} />
+              <Route path="/signup" element={<SignupForm />} />
+              <Route path="/d-login" element={<DeveloperLoginForm />} />
+              <Route path="/pm-login" element={<ProjectManagerLoginForm />} />
+            </Routes>
       </Router>
   );
 
