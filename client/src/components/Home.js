d2de26ae7716c977cb457f5e1ccc412237cc513a
Blessed <blessedallan1@gmail>
2022-12-08 10:44:15 +0300

    classes for styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import  '../Styles/Home.css'
 
 
 const Home = () => {
   return (
     <div id='homepage'>
-      <h2>
+      <h2 className= 'text'>
         Please select your position in the organization: <br />
       </h2>
-      <NavLink to='/login/developer'>I am a Developer</NavLink> <br />
-      <NavLink to='/login/project_manager'>I am a Project Manager</NavLink>
+      <NavLink
+      className="nav-item"
+      to='/login/developer'>I am a Developer</NavLink> <br />
+      <NavLink 
+      className="nav-item"
+      to='/login/project_manager'>I am a Project Manager</NavLink>
     </div>
   )
 }
