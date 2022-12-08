8cdab8a64ebfaaa78538b70dc6b398b62d593fc0
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-08 18:23:14 +0300

    Import component and add styles.

--- client/src/components/Developer.js
+++ client/src/components/Developer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import MyProjects from './MyProjects'
+import NavBar from './NavBar'
 
 function Developer( { userProps }) {
   return (
     <>
-      <div>Developer</div>
+    <div>Developer</div>
+    <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+      <NavBar />
       <MyProjects userProps = {userProps} />
+    </div>
     </>
 
   )
