8d8abc3c5ee8757f2a1e8c4c2ed2e48d1c6e3e40
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-09 12:24:24 +0300

    Fix bug.

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,5 +2,5 @@ class Developer < ApplicationRecord
     has_secure_password
     validates :password, length: {minimum: 6}
     has_many :projects
-    has_many :projects_managers, through: :projects
+    has_many :project_managers, through: :projects
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :deadline, :progress
+  attributes :id, :title, :description, :deadline, :progress, :developer_id, :project_manager_id
   belongs_to :project_manager
   belongs_to :developer
 end
