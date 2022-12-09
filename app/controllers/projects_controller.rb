9c638854a274796f70298a718d4127b1ce2ce9d7
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-09 20:33:25 +0300

    Fix bugs.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,26 @@
 class ProjectsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index 
+        render json: Project.all
+    end
+
     def create 
         project = Project.create!(project_params)
         render json: project, status: :created
     end
 
-    def index 
-        project = Project.all
-        render json: project
+
+    def destroy
+        project = Project.find(params[:id])
+        project.destroy
+        render json: { message: 'Project deleted'}, status: :no_content
     end
 
     private 
 
     def project_params
-       param.permit(:title, :description, :deadline, :progress, :project_manager_id, :developer_id)
+       params.permit(:title, :description, :deadline, :progress, :project_manager_id, :developer_id)
     end 
 
     def render_unprocessable_entity_response(invalid)
