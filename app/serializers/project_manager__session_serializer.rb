d7bf3139e6ef7f14fd2eb94ffc21138ecab8ffc8
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-09 12:02:24 +0300

    Fix bug.

--- app/serializers/project_manager__session_serializer.rb
+++ app/serializers/project_manager__session_serializer.rb
@@ -1,5 +1,3 @@
 class ProjectManagerSessionSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :profile_pic
-  has_many :projects
-  has_many :developers
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :descrption, :deadline, :progress
+  attributes :id, :title, :description, :deadline, :progress
   belongs_to :project_manager
   belongs_to :developer
 end
