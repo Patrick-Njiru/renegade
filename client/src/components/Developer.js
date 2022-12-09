cf53a4194048647e696d048474a3ffb53b3297e0
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-12-09 13:03:40 +0300

    made changes separated navbar and developer

--- client/src/components/Developer.js
+++ client/src/components/Developer.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import MyProjects from './MyProjects'
+import NavBar from './NavBar'
 
 function Developer( { userProps }) {
   return (
     <>
+      <NavBar />
       <div>Developer</div>
       <MyProjects userProps = {userProps} />
     </>
-
   )
 }
 
-export default Developer
+export default Developer;
