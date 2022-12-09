4e5ab470f578d58b17102b785283b9be3efc7ded
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-09 22:16:33 +0300

    Modify file.

--- client/src/scenes/constant/Sidebar.jsx
+++ client/src/scenes/constant/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
-import { Link } from "react-router-dom";
 import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
